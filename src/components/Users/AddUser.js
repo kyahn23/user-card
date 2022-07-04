1d3d1b23f6d917413b70e12c02922ae5d302225c
kyahn23 <kyahn23@gmail.com>
2022-07-05 01:31:33 +0900

    state 를 통한 목록 관리 / ErrorModal 컴포넌트 추가

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnterredUserName] = useState("");
   const [enteredAge, setEnterredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age(non-empty values).",
+      });
       return;
     }
     // +를 붙힘으로서 숫자형으로 만든다
     // enteredAge를 입력하는 input type이 number
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age( > 0 ).",
+      });
       return;
     }
 
@@ -30,27 +40,41 @@ const AddUser = (props) => {
     setEnterredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    // html 컴포넌트가 아닌경우
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
         />
-        <label htmlFor="age">Age (years)</label>
-        <input
-          id="age"
-          type="number"
-          value={enteredAge}
-          onChange={ageChangeHandler}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      )}
+
+      {/* html 컴포넌트가 아닌경우 */}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
