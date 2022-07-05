ae2c8eb5282a36c8b3d8a51f91533b3d0769686f
kyahn23 <kyahn23@gmail.com>
2022-07-06 00:29:23 +0900

    useRef 사용

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 import Wrapper from "../Helpers/Wrapper";
@@ -6,13 +6,16 @@ import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnterredUserName] = useState("");
-  const [enteredAge, setEnterredAge] = useState("");
+  const nameInputRef = useRef(); // useRef => current 프로퍼티를 갖는 객체
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name and age(non-empty values).",
@@ -21,7 +24,7 @@ const AddUser = (props) => {
     }
     // +를 붙힘으로서 숫자형으로 만든다
     // enteredAge를 입력하는 input type이 number
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: "Invalid age",
         message: "Please enter a valid age( > 0 ).",
@@ -29,16 +32,9 @@ const AddUser = (props) => {
       return;
     }
 
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnterredUserName("");
-    setEnterredAge("");
-  };
-  const usernameChangeHandler = (event) => {
-    setEnterredUserName(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnterredAge(event.target.value);
+    props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = ""; // DOM을 조작하기위해 ref를 사용하는 경우는 드물다
+    ageInputRef.current.value = "";
   };
 
   const errorHandler = () => {
@@ -62,16 +58,10 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            ref={nameInputRef} // ref 에 들어있는 값이 실제 DOM의 요소가 된다
           />
           <label htmlFor="age">Age (years)</label>
-          <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
